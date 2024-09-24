1df73cffaaa2f1b957ec045cd04f3d77d57786d5
Anggun Caksono <caksonoanggun@gmail.com>
2024-09-24 19:47:16 +0700

    chore: Update dependencies and add CORS support
    Update the versions of the dependencies "body-parser", "encodeurl", "express", "finalhandler", "merge-descriptors", "path-to-regexp", "qs", "send", and "serve-static" to their latest versions. Also, add CORS support to the application by including the "cors" middleware.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const newsRoutes = require('./routes/newsRoutes');
 const journalistRoutes = require('./routes/journalistRoutes');
@@ -6,6 +7,9 @@ const bodyParser = require('body-parser');
 const errorHandler = require('./utils/errorHandler');
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -15,11 +19,8 @@ app.use('/news', newsRoutes);
 app.use('/auth', authRoutes);
 app.use('/journalist', journalistRoutes);
 
-
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-}
-);
+});
 
 module.exports = app;
